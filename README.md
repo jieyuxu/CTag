058d495dcc2134b87f1b22ed86212b8cd6273e16
jieyuxu <13708149+jieyuxu@users.noreply.github.com>
2020-03-05 00:51:05 -0500

    updated sample api calls and readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # IW Junior Spring 2020
+
+## Notes:
+* Google Vision API returns better accuracy for object localization requests when images are read in as bytes instead of giving the API a URL to an image. 
--- sample_api_calls.py
+++ sample_api_calls.py
@@ -3,18 +3,55 @@
 from google.cloud import vision
 
 # sample label detection
-image_uri = 'gs://cloud-samples-data/vision/using_curl/shanghai.jpeg'
+image_uri = 'https://iiif.princeton.edu/loris/figgy_prod/cb%2F1f%2F3c%2Fcb1f3ca1536c4c7388f4f610cd0659a6%2Fintermediate_file.jp2/full/1200,/0/default.jpg'
 
 client = vision.ImageAnnotatorClient()
-image = vision.types.Image()
-image.source.image_uri = image_uri
+# image = vision.types.Image()
+# image.source.image_uri = image_uri
+
+with open('abc-jingle1.jpg', 'rb') as image_file:
+    content = image_file.read()
+
+image = vision.types.Image(content=content)
 
 response = client.label_detection(image=image)
 
+objects = client.object_localization(image=image).localized_object_annotations
+print('Number of objects found: {}'.format(len(objects)))
+for object_ in objects:
+    print('\n{} (confidence: {})'.format(object_.name, object_.score))
+    print('Normalized bounding polygon vertices: ')
+    for vertex in object_.bounding_poly.normalized_vertices:
+        print(' - ({}, {})'.format(vertex.x, vertex.y))
+
+print()
 print('Labels (and confidence score):')
 print('=' * 79)
 for label in response.label_annotations:
     print(f'{label.description} ({label.score*100.:.2f}%)')
+
+response = client.face_detection(image=image)
+faces = response.face_annotations
+
+# Names of likelihood from google.cloud.vision.enums
+likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE','LIKELY', 'VERY_LIKELY')
+print('Faces:')
+
+for face in faces:
+    print('anger: {}'.format(likelihood_name[face.anger_likelihood]))
+    print('joy: {}'.format(likelihood_name[face.joy_likelihood]))
+    print('surprise: {}'.format(likelihood_name[face.surprise_likelihood]))
+
+    vertices = (['({},{})'.format(vertex.x, vertex.y)
+        for vertex in face.bounding_poly.vertices])
+
+    print('face bounds: {}'.format(','.join(vertices)))
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
 
 # Labels (and confidence score):
 # ===============================================================================
