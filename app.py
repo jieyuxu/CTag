66ac33b43fb03f66abc0c6e3b9ab172b6c176bd6
Suki Yip <suki.yip.271@gmail.com>
2020-03-31 12:11:06 -0400

    upload images

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy_session import flask_scoped_session
 from utils.base import session_factory
 from utils.api import *
@@ -8,8 +8,14 @@ sess = flask_scoped_session(session_factory, app)
 
 @app.route('/')
 def index():
-    images = search_by_tag('truck')
-    return render_template("index.html", images = imges)
+    return render_template("index.html")
+
+@app.route('/success', methods = ['POST'])
+def success():
+    if request.method == 'POST':
+        files = request.files.getlist("files[]")
+        print(files)
+        return render_template("success.html", files = files)
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=8000, debug = True)
