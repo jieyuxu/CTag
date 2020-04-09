322cb13360b0715c5d77fe527d15fc62347c5250
Suki Yip <suki.yip.271@gmail.com>
2020-04-09 19:36:19 -0400

    heroku

--- utils/base.py
+++ utils/base.py
@@ -7,13 +7,14 @@ import pgpasslib
 Base = declarative_base()
 
 password = pgpasslib.getpass('localhost', 5555, 'qroom', 'postgres')
-if not password:
-    raise ValueError('Did not find a password in the .pgpass file')
-
-url = 'postgresql://postgres:{}@localhost:5555/iw_s20'.format(password)
+# if not password:
+#     raise ValueError('Did not find a password in the .pgpass file')
+#
+# url = 'postgresql://postgres:{}@localhost:5555/iw_s20'.format(password)
 
 
 # url = 'postgresql://localhost/iw_s20'
+url = os.environ['DATABASE_URL']
 
 engine = create_engine(url)
 session_factory = sessionmaker(bind=engine)
