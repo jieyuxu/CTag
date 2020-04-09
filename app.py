87019133464547e255f6c0fa09436651fcf146a1
Suki Yip <suki.yip.271@gmail.com>
2020-04-09 12:52:54 -0400

    CAS

--- app.py
+++ app.py
@@ -1,31 +1,60 @@
 from flask import Flask, render_template, request
+from flask import session, Response
 from flask_sqlalchemy_session import flask_scoped_session
 from utils.base import session_factory
 from utils.api import *
 from utils.googleapi import *
 from base64 import b64encode
+from CAS import CAS, login
+from CAS import login_required
 
 
 app = Flask(__name__)
+app.secret_key = 'bdfa8a83f16eb4f95dc2473fe4a50820b14e74cc70cef6ae'
 sess = flask_scoped_session(session_factory, app)
 
+########## CAS AUTHENTICATION ###########
+cas = CAS(app)
+app.config['CAS_SERVER'] = "https://fed.princeton.edu/cas/login"
+app.config['CAS_AFTER_LOGIN'] = 'caslogin'
+app.config['CAS_AFTER_LOGOUT'] = 'http://localhost:8000/caslogout'
+app.config['CAS_LOGIN_ROUTE'] = '/cas'
+#########################################
+
+def isLoggedIn():
+   if 'username' in session:
+      return True
+   return False
+
 @app.route('/')
+@app.route('/signin')
+def signin():
+    if isLoggedIn():
+      return render_template("index.html")
+    return render_template("signin.html")
+
+@app.route('/caslogin')
+def caslogin():
+   print(app.config['CAS_USERNAME_SESSION_KEY'])
+   if cas.username is not None:
+      print("user:", cas.username)
+      session['username'] = cas.username
+      print("confirming user logged into session", session['username'])
+      session.modified = True
+   return render_template("index.html")
+
+@app.route('/caslogout')
+def caslogout():
+   if isLoggedIn():
+      session.pop('username')
+      if 'admin' in session:
+          session.pop('admin')
+      session.modified = True
+   return render_template("signin.html")
+
 @app.route('/index.html')
 def index():
-    netid = "sukiy"
-    user_obj = add_add_user(netid)
-    album_obj= add_get_album("HELLO WORLD", user_obj)
-    new_obj= add_get_album("little red", user_obj)
-
-    with open("photo.jpeg", "rb") as image:
-      f = base64.b64encode(image.read())
-
-    img1 = Images(album=album_obj, picture=f)
-    change_album(img1, new_obj)
-
-
-    test = images_album(album_obj)
-    return render_template("index.html")
+  return render_template("index.html")
 
 # add images
 @app.route('/success', methods = ['POST'])
