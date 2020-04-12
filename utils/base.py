ff122a90cc4ca2d3c5ac55add5d3ed99dcceb860
Suki Yip <suki.yip.271@gmail.com>
2020-04-12 19:00:06 -0400

    base

--- utils/base.py
+++ utils/base.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import pgpasslib
+import os
 
 
 Base = declarative_base()
