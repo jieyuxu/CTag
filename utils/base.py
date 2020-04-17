65a32af1a6727214ef6f3da66e5ff49f7440126d
jieyuxu <13708149+jieyuxu@users.noreply.github.com>
2020-04-16 22:55:24 -0400

    resolved merge error

--- utils/base.py
+++ utils/base.py
@@ -10,9 +10,8 @@ Base = declarative_base()
 # password = pgpasslib.getpass('localhost', 5555, 'qroom', 'postgres')
 # if not password:
 #     raise ValueError('Did not find a password in the .pgpass file')
-#
-# url = 'postgresql://postgres:{}@localhost:5555/iw_s20'.format(password)
 
+# url = 'postgresql://postgres:{}@localhost:5555/iw_s20'.format(password)
 
 # url = 'postgresql://localhost/iw_s20'
 url = os.environ['DATABASE_URL']
