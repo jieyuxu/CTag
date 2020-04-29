b4fbc63bb23961c2e44d8bbb9e0527ef211df9d8
jieyuxu <13708149+jieyuxu@users.noreply.github.com>
2020-04-28 22:54:34 -0400

    added detect web uri func

--- utils/googleapi.py
+++ utils/googleapi.py
@@ -137,3 +137,69 @@ def detect_labels_bytestream(bytestream):
                 response.error.message))
 
     return label_set
+
+def detect_web_uri(uri):
+    """Detects web annotations in the file located in Google Cloud Storage."""
+    client = vision.ImageAnnotatorClient()
+    image = vision.types.Image()
+    image.source.image_uri = uri
+
+    response = client.web_detection(image=image)
+    annotations = response.web_detection
+
+    # best_guess_labels = set()
+
+    # if annotations.best_guess_labels:
+    #     for label in annotations.best_guess_labels:
+    #         print('\nBest guess label: {}'.format(label.label))
+    #         best_guess_labels.add(label.label)
+
+    # if annotations.pages_with_matching_images:
+    #     print('\n{} Pages with matching images found:'.format(
+    #         len(annotations.pages_with_matching_images)))
+
+    #     for page in annotations.pages_with_matching_images:
+    #         print('\n\tPage url   : {}'.format(page.url))
+
+    #         if page.full_matching_images:
+    #             print('\t{} Full Matches found: '.format(
+    #                    len(page.full_matching_images)))
+
+    #             for image in page.full_matching_images:
+    #                 print('\t\tImage url  : {}'.format(image.url))
+
+    #         if page.partial_matching_images:
+    #             print('\t{} Partial Matches found: '.format(
+    #                    len(page.partial_matching_images)))
+
+    #             for image in page.partial_matching_images:
+    #                 print('\t\tImage url  : {}'.format(image.url))
+
+    # maps description to score
+    # best_guess_description = dict()
+
+    # if annotations.web_entities:
+    #     print('\n{} Web entities found: '.format(
+    #         len(annotations.web_entities)))
+
+    #     for entity in annotations.web_entities:
+    #         best_guess_description[entity.description] = entity.score
+    #         print('\n\tScore      : {}'.format(entity.score))
+    #         print(u'\tDescription: {}'.format(entity.description))
+
+    similar_images = set()
+    if annotations.visually_similar_images:
+        print('\n{} visually similar images found:\n'.format(
+            len(annotations.visually_similar_images)))
+
+        for image in annotations.visually_similar_images:
+            similar_images.add(image.url)
+            print('\tImage url    : {}'.format(image.url))
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+    
+    return similar_images
